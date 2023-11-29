c1be6eab1ce978b762e8aac23ddd4840121f994b
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-11-29 08:26:52 +0300

    task3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise
+  .all([uploadPhoto(), createUser()])
+  .then((arr) => console.log(`${arr[0].body} ${arr[1].firstName} ${arr[1].lastName}`))
+  .catch(() => console.log(new Error('Signup system offline')));
+}
