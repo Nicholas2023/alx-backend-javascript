d458beb1e1d0d95e0d4de18a0e19d6020c4df654
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-11-30 14:53:36 +0300

    task5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,16 +3,18 @@ export default class Building {
     this.sqft = sqft;
     if (this.constructor !== Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error('Class extending Building must overide evacuationWarningMessage');
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
       }
     }
   }
 
-  set sqft(newSqft) {
-    this._sqft = newSqft;
-  }
-
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
   }
 }
