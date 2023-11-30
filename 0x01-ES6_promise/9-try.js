cd71e10b0038356d68d17aebdd1a0eabbab6da4c
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-11-30 03:55:11 +0300

    task9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,14 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const rslt = mathFunction();
+    queue.push(rslt);
+  } catch (error) {
+    queue.push(error.message);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
