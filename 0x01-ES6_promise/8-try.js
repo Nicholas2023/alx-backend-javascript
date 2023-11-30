3537edce6b60607e7b917418a5aa3c086872b878
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-11-30 03:17:03 +0300

    task8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -3,6 +3,6 @@ export default function divideFunction(numerator, denominator) {
     const quotient = numerator / denominator;
     return quotient;
   } else {
-    throw new Error("cannot divide by 0");
+    throw new Error('cannot divide by 0');
   }
 }
