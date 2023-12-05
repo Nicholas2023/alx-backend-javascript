a4b285c83bd5b6808547ac89a3a1721555bbbca8
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-12-05 06:11:01 +0300

    task9

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,10 @@
 export default function groceriesList() {
   const groceries = [
-    ['Apple', 10],
+    ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
     ['Banana', 5],
   ];
-
   return new Map(groceries);
 }
