2e538b975c52bad60d15578dbefb86e9cdb28f76
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-12-07 02:44:35 +0300

    task0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,81 @@
+export interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: 'Nicky',
+  lastName: 'Mane',
+  age: 10,
+  location: 'Kibra',
+};
+
+const student2: Student = {
+  firstName: 'Anna',
+  lastName: 'Muturi',
+  age: 7,
+  location: 'Jamhuri Estate',
+};
+
+const studentList: Array<Student> = [student1, student2];
+
+const styleSheet = `
+  html {
+    margin: 0;
+    height: 100%;
+  }
+  body {
+    box-sizing: border-box;
+    display: flex;
+    align-item: center;
+    justify-content: center;
+    height: 80%;
+    margin: 10%;
+  }
+  table {
+    border-collapse: collapse;
+  }
+  thead {
+    font-weight: bold;
+  }
+  td {
+    padding: 10px;
+    border: 1px, solid gray;
+    cursor: pointer;
+  }
+  td:hover {
+    background: gainsboro;
+  }
+  td:nth-child(1) {
+    text-align: center;
+  }`;
+
+export const renderTable = (students: Array<Student>): void => {
+  const table = document.createElement('table');
+  const tableHead = document.createElement('thead');
+  const headRow = document.createElement('tr');
+  const tableBody = document.createElement('tbody');
+  headRow.insertAdjacentHTML('beforeend', `<td>FirstName</td>`);
+  headRow.insertAdjacentHTML('beforeend', `<td>Location</td>`);
+  tableHead.insertAdjacentElement('beforeend', headRow);
+
+  for (const student of students) {
+    const bodyRow = document.createElement('tr');
+    bodyRow.insertAdjacentHTML('beforeend', `<td>${student.firstName}</td>`);
+    bodyRow.insertAdjacentHTML('beforeend', `<td>${student.location}</td>`);
+    tableBody.insertAdjacentElement('beforeend', bodyRow);
+  }
+
+  table.insertAdjacentElement('beforeend', tableHead);
+  table.insertAdjacentElement('beforeend', tableBody);
+  document.body.insertAdjacentElement('beforeend', table);
+};
+
+renderTable(studentList);
+
+const styleSheetElement = document.createElement('style');
+styleSheetElement.innerHTML = styleSheet;
+document.head.insertAdjacentElement('beforeend', styleSheetElement);
+document.title = 'task 0';
