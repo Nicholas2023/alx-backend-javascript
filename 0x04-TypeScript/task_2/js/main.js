5155f77ca1cb03359775fba9ee42ba4ba3e70087
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-12-07 03:37:12 +0300

    task2

---
+++ 0x04-TypeScript/task_2/js/main.js
@@ -0,0 +1,52 @@
+var Director = /** @class */ (function () {
+    function Director() {
+    }
+    Director.prototype.workFromHome = function () {
+        return 'Working from Home';
+    };
+    Director.prototype.getCoffeeBreak = function () {
+        return 'Getting a coffee break';
+    };
+    Director.prototype.workDirectorTasks = function () {
+        return 'Getting to director tasks';
+    };
+    return Director;
+}());
+;
+var Teacher = /** @class */ (function () {
+    function Teacher() {
+    }
+    Teacher.prototype.workFromHome = function () {
+        return 'Cannot work from Home';
+    };
+    Teacher.prototype.getCoffeeBreak = function () {
+        return 'Cannot have a break';
+    };
+    Teacher.prototype.workTeacherTasks = function () {
+        return 'Getting to work';
+    };
+    return Teacher;
+}());
+function createEmployee(salary) {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+}
+function isDirector(employee) {
+    return employee instanceof Director;
+}
+function executeWork(employee) {
+    if (employee instanceof Director)
+        employee.workDirectorTasks();
+    else if (employee instanceof Teacher)
+        employee.workTeacherTasks();
+}
+function teachClass(todayClass) {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    }
+    if (todayClass === 'History') {
+        return 'Teaching History';
+    }
+}
