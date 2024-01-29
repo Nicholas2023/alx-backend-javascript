2aec9602dc8a0cdae3ec6c8f41e6d0b61f121b62
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2024-01-29 22:47:45 +0300

    task0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message to STDOUT
+ * @param {str} msg to be displayed
+ * @authour Nicholas Otieno <https://github.com/Nicholas2023>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
